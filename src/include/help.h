d4f973907faac1471e222565f72b2cb5c4b6c907
Kostiantyn Naddur <kostianaddur@gmail.com>
2022-06-27 16:41:51 +0300

    includes

---
+++ src/include/help.h
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<readline/readline.h>
+#include<readline/history.h>
+
+// Include
+
+void openHelp()
+{
+    puts("\n***WELCOME TO SHELL HELP***"
+        "\nList of Commands supported:"
+        "\n>cd"
+        "\n>ls"
+        "\n>exit"
+        "\n>all other general commands available in UNIX shell"
+        "\n>pipe handling"
+        "\n>improper space handling");
+  
+    return;
+}
