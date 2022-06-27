4ca9cc6ff875adf41428d887956a9bcbb7234c4f
Kostiantyn Naddur <kostianaddur@gmail.com>
2022-06-27 16:41:40 +0300

    main

--- src/main.c
+++ src/main.c
@@ -7,6 +7,13 @@
 #include<sys/wait.h>
 #include<readline/readline.h>
 #include<readline/history.h>
+
+// Includes
+
+#include "include/help.h"
+#include "include/commands.h"
+#include "include/error.h"
+
   
 #define MAXCOM 1000
 #define MAXLIST 100
@@ -36,7 +43,7 @@ int takeInput(char* str)
 {
     char* buf;
   
-    buf = readline("\n>>> ");
+    buf = readline("\n: ");
     if (strlen(buf) != 0) {
         add_history(buf);
         strcpy(str, buf);
@@ -76,117 +83,19 @@ void execArgs(char** parsed)
 }
   
 // Function where the piped system commands is executed
-void execArgsPiped(char** parsed, char** parsedpipe)
-{
-    // 0 is read end, 1 is write end
-    int pipefd[2]; 
-    pid_t p1, p2;
-  
-    if (pipe(pipefd) < 0) {
-        printf("\nPipe could not be initialized");
-        return;
-    }
-    p1 = fork();
-    if (p1 < 0) {
-        printf("\nCould not fork");
-        return;
-    }
-  
-    if (p1 == 0) {
-        // Child 1 executing..
-        // It only needs to write at the write end
-        close(pipefd[0]);
-        dup2(pipefd[1], STDOUT_FILENO);
-        close(pipefd[1]);
-  
-        if (execvp(parsed[0], parsed) < 0) {
-            printf("\nCould not execute command 1..");
-            exit(0);
-        }
-    } else {
-        // Parent executing
-        p2 = fork();
-  
-        if (p2 < 0) {
-            printf("\nCould not fork");
-            return;
-        }
-  
-        // Child 2 executing..
-        // It only needs to read at the read end
-        if (p2 == 0) {
-            close(pipefd[1]);
-            dup2(pipefd[0], STDIN_FILENO);
-            close(pipefd[0]);
-            if (execvp(parsedpipe[0], parsedpipe) < 0) {
-                printf("\nCould not execute command 2..");
-                exit(0);
-            }
-        } else {
-            // parent executing, waiting for two children
-            wait(NULL);
-            wait(NULL);
-        }
-    }
+void error(char** parsed, char** parsedpipe){
+    execArgsPiped(parsed, parsedpipe);
 }
   
 // Help command builtin
-void openHelp()
-{
-    puts("\n***WELCOME TO MY SHELL HELP***"
-        "\n-Use the shell at your own risk..."
-        "\nList of Commands supported:"
-        "\n>cd"
-        "\n>ls"
-        "\n>exit"
-        "\n>all other general commands available in UNIX shell"
-        "\n>pipe handling"
-        "\n>improper space handling");
-  
-    return;
+void help(){
+    openHelp();
 }
   
 // Function to execute builtin commands
-int ownCmdHandler(char** parsed)
+int commands(char** parsed)
 {
-    int NoOfOwnCmds = 4, i, switchOwnArg = 0;
-    char* ListOfOwnCmds[NoOfOwnCmds];
-    char* username;
-  
-    ListOfOwnCmds[0] = "exit";
-    ListOfOwnCmds[1] = "cd";
-    ListOfOwnCmds[2] = "help";
-    ListOfOwnCmds[3] = "hello";
-  
-    for (i = 0; i < NoOfOwnCmds; i++) {
-        if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0) {
-            switchOwnArg = i + 1;
-            break;
-        }
-    }
-  
-    switch (switchOwnArg) {
-    case 1:
-        printf("\nGoodbye\n");
-        exit(0);
-    case 2:
-        chdir(parsed[1]);
-        return 1;
-    case 3:
-        openHelp();
-        return 1;
-    case 4:
-        username = getenv("USER");
-        printf("\nHello %s.\nMind that this is "
-            "not a place to play around."
-            "\nUse help to know more..\n",
-            username);
-        return 1;
-    default:
-        break;
-    }
-  
-    return 0;
+    ownCmdHandler(parsed);
 }
   
 // function for finding pipe
@@ -252,20 +161,13 @@ int main()
     init_shell();
   
     while (1) {
-        // print shell line
         printDir();
-        // take input
         if (takeInput(inputString))
             continue;
-        // process
+
         execFlag = processString(inputString,
         parsedArgs, parsedArgsPiped);
-        // execflag returns zero if there is no command
-        // or it is a builtin command,
-        // 1 if it is a simple command
-        // 2 if it is including a pipe.
-  
-        // execute
+
         if (execFlag == 1)
             execArgs(parsedArgs);
   
