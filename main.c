ae37cc00986f8d08e76ae643a5a55d249b30b4f8
Kostiantyn Naddur <kostianaddur@gmail.com>
2022-07-05 00:09:12 +0300

    Main Code

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-// C Program to design a shell in Linux
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -8,38 +7,36 @@
 #include<readline/readline.h>
 #include<readline/history.h>
 
+// Includes
+
 #include "include/help.h"
 #include "include/commands.h"
 #include "include/error.h"
 #include "include/dir.h"
 #include "include/execargs.h"
 
-  
+
 #define MAXCOM 1000
 #define MAXLIST 100
-  
+
 #define clear() printf("\033[H\033[J")
-  
+
 void init_shell()
 {
     clear();
-    printf("\n\n\n\n******************"
-        "************************");
-    printf("\n\n\n\t****LCS SHELL****");
-    printf("\n\n\n\n*******************"
-        "***********************");
+    printf("\nLCS SHELL");
     char* username = getenv("USER");
     printf("\n\n\nUSER: @%s", username);
     printf("\n");
     sleep(2);
     clear();
 }
-  
+
 int takeInput(char* str)
 {
     char* buf;
-  
-    buf = readline("\n: ");
+
+    buf = readline(":~ $ ");
     if (strlen(buf) != 0) {
         add_history(buf);
         strcpy(str, buf);
@@ -48,16 +45,16 @@ int takeInput(char* str)
         return 1;
     }
 }
-  
+
 void dir(){
     printDir();
 }
-  
+
 void args(char** parsed)
 {
     execArgs(parsed);
 }
-  
+
 void error(char** parsed, char** parsedpipe){
     execArgsPiped(parsed, parsedpipe);
 }
@@ -70,7 +67,7 @@ int commands(char** parsed)
 {
     ownCmdHandler(parsed);
 }
-  
+
 int parsePipe(char* str, char** strpiped)
 {
     int i;
@@ -79,58 +76,58 @@ int parsePipe(char* str, char** strpiped)
         if (strpiped[i] == NULL)
             break;
     }
-  
+
     if (strpiped[1] == NULL)
         return 0;
     else {
         return 1;
     }
 }
-  
+
 void parseSpace(char* str, char** parsed)
 {
     int i;
-  
+
     for (i = 0; i < MAXLIST; i++) {
         parsed[i] = strsep(&str, " ");
-  
+
         if (parsed[i] == NULL)
             break;
         if (strlen(parsed[i]) == 0)
             i--;
     }
 }
-  
+
 int processString(char* str, char** parsed, char** parsedpipe)
 {
-  
+
     char* strpiped[2];
     int piped = 0;
-  
+
     piped = parsePipe(str, strpiped);
-  
+
     if (piped) {
         parseSpace(strpiped[0], parsed);
         parseSpace(strpiped[1], parsedpipe);
-  
+
     } else {
-  
+
         parseSpace(str, parsed);
     }
-  
+
     if (ownCmdHandler(parsed))
         return 0;
     else
         return 1 + piped;
 }
-  
+
 int main()
 {
     char inputString[MAXCOM], *parsedArgs[MAXLIST];
     char* parsedArgsPiped[MAXLIST];
     int execFlag = 0;
     init_shell();
-  
+
     while (1) {
         printDir();
         if (takeInput(inputString))
@@ -141,10 +138,9 @@ int main()
 
         if (execFlag == 1)
             execArgs(parsedArgs);
-  
+
         if (execFlag == 2)
             execArgsPiped(parsedArgs, parsedArgsPiped);
     }
     return 0;
 }
-
